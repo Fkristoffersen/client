2b36b695b01739f17d8ecbf0db31b4533d0ae5c7
Frederik Kristoffersen <Frederikkristoffersen95@gmail.com>
2022-06-30 10:32:45 +0200

    onChange Events

--- src/App.js
+++ src/App.js
@@ -1,25 +1,59 @@
-import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
 
 function App() {
+  const [FirstName, setFirstName] = useState('');
+  const [LastName, setLastName] = useState('');
+  const [Address, setAddress] = useState('');
+  const [Postal, setPostal] = useState('');
+  const [Phone, setPhone] = useState('');
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="information">
+        <label>FirstName:</label>
+        <input
+          type="text"
+          onChange={(event) => {
+            setFirstName(event.target.value);
+          }}
+        />
+
+        <label>LastName:</label>
+        <input
+          type="text"
+          onChange={(event) => {
+            setLastName(event.target.value);
+          }}
+        />
+
+        <label>Address:</label>
+        <input
+          type="text"
+          onChange={(event) => {
+            setAddress(event.target.value);
+          }}
+        />
+
+        <label>Postal:</label>
+        <input
+          type="text"
+          onChange={(event) => {
+            setPostal(event.target.value);
+          }}
+        />
+
+        <label>Phone:</label>
+        <input
+          type="text"
+          onChange={(event) => {
+            setPhone(event.target.value);
+          }}
+        />
+
+        <button>Add</button>
+      </div>
     </div>
   );
 }
-
 export default App;
